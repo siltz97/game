3d29aebbfa1d69752a0389b4525fc78492975240
DESKTOP-EJH500L\aa <sshmashur@fbk.eu>
2025-04-15 09:35:57 +0200

    test(inventory) add tests for new algorithm that changes price based on reputation

--- src/test/java/my/fbk/npc/MerchantTest.java
+++ src/test/java/my/fbk/npc/MerchantTest.java
@@ -2,6 +2,8 @@ package my.fbk.npc;
 
 import my.fbk.npc.all_npc.Merchant;
 import my.fbk.npc.effects.InvisibilityAbstractEffect;
+import my.fbk.npc.inventory.ItemList;
+import my.fbk.npc.my_player.Player;
 import my.fbk.npc.speak.SilentSpeak;
 import my.fbk.npc.factories.NPCFactory;
 import org.junit.jupiter.api.Assertions;
@@ -13,26 +15,26 @@ import org.junit.jupiter.api.Test;
 public class MerchantTest {
 
     @Test
-    public void merchantTestPositive() {
+    public void merchantTestPositiveBehavior() {
         Merchant merchant = new Merchant(1, 100, 100, 2,1,0);
         merchant.speak();
     }
 
     @Test
-    public void merchantTestNeutral() {
+    public void merchantTestNeutralBehavior() {
         Merchant merchant = new Merchant(1, 100, 61, 2,1,0);
         merchant.speak();
     }
 
     @Test
-    public void merchantTestAggressive() {
+    public void merchantTestAggressiveBehavior() {
         Merchant merchant = new Merchant(1, 100, 40, 2,1,0);
         merchant.speak();
         Assertions.assertFalse(merchant.getReputation() > 50 && merchant.getAbstractEffects().isEmpty());
     }
 
     @Test
-    public void merchantTestSilent() {
+    public void merchantTestSilentBehavior() {
         Merchant merchant = new Merchant(1, 100, 100, 2,1,0);
         merchant.setAbstractEffects(new InvisibilityAbstractEffect(5));
         merchant.speak();
@@ -42,11 +44,11 @@ public class MerchantTest {
     @Test
     @DisplayName("user can trade")
     public void merchantTestInventoryAccessConditions() {
-        Merchant merchant = new Merchant(1, 100, 100, 2,1,0);
-        if (merchant.getReputation() > 50 && merchant.getAbstractEffects().isEmpty()) {
+        Merchant merchant = NPCFactory.makeMerchant();
+        if (merchant.getReputation() > 30 && merchant.getAbstractEffects().isEmpty()) {
             merchant.showInventory();
         }
-        Assertions.assertTrue(merchant.getReputation() > 50 && merchant.getAbstractEffects().isEmpty());
+        Assertions.assertTrue(merchant.getReputation() > 30 && merchant.getAbstractEffects().isEmpty());
     }
     @Test
     public void merchantTestInventoryHasItems() {
@@ -54,5 +56,24 @@ public class MerchantTest {
         merchant.showInventory();
         Assertions.assertTrue(merchant.getInventory().getInventorySize()>0,"good");
     }
+    @Test
+    @DisplayName("Buy items with increased price")
+    public void merchantTestInventoryChangePriseBasedOnReputationBuy() {
+        Merchant merchant =  NPCFactory.makeMerchant();
+        merchant.setReputation(90);
+        merchant.getInventory().tradeBasedOnReputationBuy(merchant);
+        merchant.showInventory();
+    }
+    @Test
+    @DisplayName("Sell items with decreased price")
+    public void merchantTestInventoryChangePriseBasedOnReputationSell() {
+        Merchant merchant =  NPCFactory.makeMerchant();
+        Player player = new Player(1000, 100, 100, 2,1,1);
+        merchant.setReputation(90);
+        player.getInventory().addItemToInventory(ItemList.fireScroll());
+        player.getInventory().tradeBasedOnReputationSell(merchant,player);
+        player.showInventory();
+    }
+
 
 }
